f14d3eda06381e19316d4c3a118e1f7ee6919d74
gagude <55392342+gagude@users.noreply.github.com>
2020-11-24 19:29:50 -0300

    Add_Tickets_By_ID

--- tickets/views.py
+++ tickets/views.py
@@ -43,27 +43,95 @@ def add_tickets(request):
         return HttpResponseRedirect(reverse("login"))
     else:
         if request.method == 'POST':
-
+            print('Form POST')
             form = AddTicket(request.POST or None)
 
-
             if form.is_valid():
+                print('Form Valid')
                 form.save()
                 form.full_clean()
+            else:
+                print("NÃO")
+        try:
+            isNumber = ''+str(request)
+            isNumber = isNumber.split('?')[1]
+            isNumber = isNumber.split('\'')[0]
+        except:
+            print(isNumber+'Is Not a Number')
+        
         form = AddTicket()
         context['page'] = 'Cadastro Ticket'
         context['form'] = form
-        context['today'] = date.today().strftime("%d/%m/%Y")
+        
         #calculations = Calculations()
+        context['today'] = date.today().strftime("%Y-%m-%d")
+        context['responsavel'] = request.user
         context['cont_ticket'] += 1
         #cont = calculations.calc_cont(Tickets.objects.all())
         context['protocolo'] = date.today().strftime("%Y%m%d")+ "%03d" % request.user.id + "%03d" % context['cont_ticket']
+        context['requestsID'] = isNumber
+        if isNumber.isnumeric():
+                print('NUMERO')
+                based = Tickets.objects.all()
+                for itens in based:
+                    if str(itens.id) == str(isNumber):
+                        context['today'] = itens.data_abertura
+                        context['empresa'] = itens.empresa
+                        context['responsavel'] = itens.responsavel
+                        context['cliente'] = itens.cliente
+
+                return render(request, "tickets/add_ticket_POP.html",context)
+        return render(request, "tickets/add_ticket.html",context)
 
 
+def add_tickets_pop(request):
+        try:
+            isNumber = ''+str(request)
+            isNumber = isNumber.split('?')[1]
+            isNumber = isNumber.split('\'')[0]
+        except:
+            print(isNumber+'Is Not a Number')
+
+        if request.method == 'POST':
+            print('Form POST')
+            form = AddTicket(request.POST or None)
+            print(form.data)
+            if form.is_valid():
+                print('Form Valid')
+                Tickets.objects.filter(id = isNumber).update(assunto = form.data['assunto'])
+                Tickets.objects.filter(id = isNumber).update(service = form.data['service'])
+                Tickets.objects.filter(id = isNumber).update(tipo = form.data['tipo'])
+                Tickets.objects.filter(id = isNumber).update(status = form.data['status'])
+                Tickets.objects.filter(id = isNumber).update(descri = form.data['descri'])
+
+        based = Tickets.objects.all()
+        for itens in based:
+                    if str(itens.id) == str(isNumber):
+                        context['today'] = itens.data_abertura.strftime("%Y-%m-%d")
+                        print(itens.data_abertura)
+                        context['empresa'] = itens.empresa
+                        context['responsavel'] = itens.responsavel
+                        context['cliente'] = itens.cliente
+                        context['id_lig'] = itens.id_ligacao
+
+        form = AddTicket()
+        context['page'] = 'Cadastro Ticket'
+        context['form'] = form
+        print()
+        #calculations = Calculations()
+        
+        
+        context['cont_ticket'] += 1
+        #cont = calculations.calc_cont(Tickets.objects.all())
+        context['protocolo'] = date.today().strftime("%Y%m%d")+ "%03d" % request.user.id + "%03d" % context['cont_ticket']
+        context['requestsID'] = isNumber
+        if isNumber.isnumeric():
+                print('NUMERO')
+                
+
+                return render(request, "tickets/add_ticket_POP.html",context)
         return render(request, "tickets/add_ticket_POP.html",context)
-
-
-
+        
 
 def relatorio_tickets(request):
     if not request.user.is_authenticated:
